471a5a8f707155bc75486f4a5b26b08a8523f430
Philix27 <philixbob@gmail.com>
2022-02-15 06:31:46 +0100

    package.json

--- comps/globalComps/Footer/Footer.js
+++ comps/globalComps/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './Footer.module.css'
 import { Section, Linka } from './footerStyles';
-import { FaWhatsapp } from 'react-icons/fa'
 import { AiOutlineMail } from 'react-icons/ai'
 import { IoLogoGoogle } from 'react-icons/io'
 
