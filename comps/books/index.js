4d904cb43ff5c880de73d312e23d48de7bfb8dbd
Philix27 <philixbob@gmail.com>
2022-09-16 10:26:13 +0100

    made note card clickable

--- comps/books/index.js
+++ comps/books/index.js
@@ -48,30 +48,32 @@ export default function BooksComp({ posts, title, page, categoryList }) {
       {/* //! Consider designing a dropdown select option especially for mobile view */}
       <div className="gridContainer">
         {selectedCategory.map((post, index) => (
-          <motion.div
-            key={index}
-            className="card"
-            initial={{ x: "-100vw", opacity: 0.1 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              duration: 1.7,
-              delay: 1,
-              type: "spring",
-              stiffness: "110",
-            }}
-          >
-            {/* transition={{duration: 1.7, delay: 1, type: "tween", }}> */}
-            {/* <Image src={image} width={500} height={300}/> */}
-            {/* <Link href={`/${page}/${post.slug}`} target="_blank">
+          <Link href={`/${page}/${post.slug}`} target="_blank">
+            <motion.div
+              key={index}
+              className="card"
+              initial={{ x: "-100vw", opacity: 0.1 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                duration: 1.7,
+                delay: 1,
+                type: "spring",
+                stiffness: "110",
+              }}
+            >
+              {/* transition={{duration: 1.7, delay: 1, type: "tween", }}> */}
+              {/* <Image src={image} width={500} height={300}/> */}
+              {/* <Link href={`/${page}/${post.slug}`} target="_blank">
               <img className="img" src={post.frontmatter.cover_image} />
             </Link> */}
 
-            <div className="texty">
-              <h3 className="bookTitle">
-                {index + 1}. {post.frontmatter.title}
-              </h3>
-            </div>
-          </motion.div>
+              <div className="texty">
+                <h3 className="bookTitle">
+                  {index + 1}. {post.frontmatter.title}
+                </h3>
+              </div>
+            </motion.div>
+          </Link>
         ))}
       </div>
     </div>
